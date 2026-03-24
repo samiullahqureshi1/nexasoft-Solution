1eb45dd69d00dcd48268142a297f87e9ebf5c060
samiullahqureshi1 <samiullahqureshi669@gmail.com>
2026-03-24 10:05:51 -0700

    done

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2015', // transpile modern JS so react-snap can parse it
+    outDir: 'dist',   // default, optional
+  },
 })
